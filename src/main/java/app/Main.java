7fede7ecc40589798aaebf03f543e99c80b685f6
Vladimir Postman <v.postman2014@yandex.com>
2018-04-14 21:25:40 +0300

    changes for home work in part 01

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,18 +3,22 @@ package app;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 import servlets.AllRequestsServlet;
+import servlets.ServletRequestMirror;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        ServletRequestMirror servletRequestMirror = new ServletRequestMirror();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        //context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+        context.addServlet(new ServletHolder(servletRequestMirror), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
-
         server.start();
+        System.out.println("Server started");
         server.join();
+
     }
 }
